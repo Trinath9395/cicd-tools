a38d3fd721ecb669084812caf9f8a768544a53bc
Trinath9395 <trinathdevops0606@gmail.com>
2025-05-12 09:38:29 +0530

    jenkins

--- main.tf
+++ main.tf
@@ -1,47 +1,49 @@
 module "jenkins" {
-  source = "terraform-aws-module/ec2-instance/aws"
+  source = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 5.0"
 
   name = "jenkins"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0e142280b2978f20a"]
+  subnet_id = "subnet-0452ce0b87e65b4ee"
+  ami = data.aws_ami.ami_info.id
+  user_data = file("jenkins.sh")
 
-  instance_type       = "t3.small"
-  vpc_security_group  = ["sg-0e142280b2978f20a"]
-  subnet_id           = "subnet-0452ce0b87e65b4ee"
-  ami                 = data.aws_ami.ami_info.id 
-  user_data           = file("jenkins.sh")
+  root_block_device = [
+    {
+      volume_size = 50
+      volume_type = "gp3"
+      delete_on_termination = true
+    }
+  ]
+
   tags = {
     Name = "jenkins"
   }
-
-  root_block_device  = [
-    {
-        volume_size = 50 
-        volume_type = "gp3"
-        delete_on_protection = true
-    }
-  ]
 }
 
 module "jenkins-agent" {
-    source = "terraform-aws-module/ec2-instance/aws"
+  source = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 5.0"
 
-    name = "jenkins-agent"
+  name = "jenkins-agent"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0e142280b2978f20a"]
+  subnet_id = "subnet-0452ce0b87e65b4ee"
+  ami = data.aws_ami.ami_info.id
+  user_data = file("jenkins-agent.sh")
 
-    instance_type = "t3.small"
-    vpc_security_group =  ["sg-0e142280b2978f20a"]
-    subnet_id = "subnet-0452ce0b87e65b4ee"
-    ami = data.aws_ami.ami_info.id
-    user_data = file("jenkins-agent.sh")
-    tags = {
-        Name = "jenkins-agent"
+  root_block_device = [
+    {
+      volume_size = 50
+      volume_type = "gp3"
+      delete_on_termination = true
     }
+  ]
 
-    root_block_device = [
-        {
-            volume_size = 50
-            volume_type = "gp3"
-            delete_on_protection = true 
-        }
-    ]
+  tags = {
+    Name = "jenkins-agent"
+  }
 }
 
 module "records" {
@@ -55,20 +57,15 @@ module "records" {
       name    = "jenkins"
       type    = "A"
       ttl     = 1
-      records = [
-        module.jenkins.public_ip
-      ]
+      records = [module.jenkins.public_ip]
       allow_overwrite = true
     },
     {
       name    = "jenkins-agent"
       type    = "A"
       ttl     = 1
-      records = [
-        module.jenkins_agent.private_ip
-      ]
+      records = [module.jenkins-agent.private_ip]
       allow_overwrite = true
     }
   ]
-
 }
